a0928c4a386e7291dc76815db62d65f0beaa0dfd
GokuProgramación <verafrancomiguel1d@gmail.com>
2023-03-21 18:26:08 -0600

    v1.2, se integro una interfaz Joption y bucles, arrerglando Bugs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
-
+/*revision : le hacia falta un menu, el cual se agrego , ademas de un error en el bucle while
+ se agregaron mensajes al memento de teclear mal un valor no valido o valido
+ un menu sencillo donde el usuario pueda interactuar
+ mas funcionalidades basicas y errores en repeticiones de datos
+autor: Miguel Vera Franco 
+*/
 public class Main {
     public static void main(String[] args) {
         hasOne h= new hasOne();
--- src/main/java/org/example/hasOne.java
+++ src/main/java/org/example/hasOne.java
@@ -5,22 +5,60 @@ import java.util.Scanner;
 
 public class hasOne {
     public void ingresarDatos() {
-        String n;
-        n = JOptionPane.showInputDialog("ingresa un Numero");
-        validar_Numero(n);
+
+        MenuDeSeleccion();
+        //n = JOptionPane.showInputDialog("ingresa un Numero");
+        //validar_Numero(n);
 
     }
+
+    public void MenuDeSeleccion() {
+
+        int opcion;
+        String respuesta;
+        respuesta = JOptionPane.showInputDialog("Bienvenido\n ¿Que deseas realizar?\n" +
+                "1 verificar si los digitos ingresados tienen un 1\n" +
+                "2 salir del programa");
+        opcion = Integer.parseInt(respuesta);
+        switch (opcion) {
+            case 1:
+                String n = null;
+                do {
+                    n = JOptionPane.showInputDialog("si deseas detener el programa escribe \n exit\n ingresa un Numero");
+
+                    if (n.equals("exit")) {
+                        System.exit(0);
+                    } else
+                        validar_Numero(n);
+                } while (n != "exit");
+
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "gracias por usarnos");
+                System.exit(0);
+                break;
+
+        }
+
+    }
+
 
     public boolean validar_Numero(String n) {
         int n1;
         boolean isNumeric = (n != null && n.matches("[0.01-9.99]+"));// valida que el valor ingresado sea un numero.
         if (isNumeric == true) {
             n1 = Integer.parseInt(n);
-            System.out.println("es un numero" + isNumeric);
+            JOptionPane.showMessageDialog(null,
+                    "Felicidades!!\n" +
+                            " El valor ingresado cuenta almenos un 1" +
+                            "\n el numero ingresado fue: " + n1);
+            // System.out.println("es un numero" + isNumeric);
             hasOne(n1);
             return true;
         } else {
-            System.out.println("no es un numero" + isNumeric);
+            JOptionPane.showMessageDialog(null, "Lo sentimos," +
+                    "\n pero " + n + "no es un numero\n" +
+                    "\n \npor favor ingrese un numero");
             return false;
         }
 
